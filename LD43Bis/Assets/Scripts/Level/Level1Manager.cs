2c2b2c1ed184cb7bf4c455259df69267ef34744a
Unknown <sigrid.c@hotmail.fr>
2018-12-05 13:23:32 +0100

    Menu of title scene creation

---
+++ LD43Bis/Assets/Scripts/Level/Level1Manager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Manager : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 spawnPosition;
+
+    [SerializeField]
+    private GameObject player;
+    private GameObject instancePlayer;
+
+
+    private void Awake()
+    {
+        instancePlayer = Instantiate(player, new Vector3(spawnPosition.x, spawnPosition.y, 0), Quaternion.identity);
+        instancePlayer.name = "Player";
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
